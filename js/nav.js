577ba3339a41907595fa471ed7dd85c115811029
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-07-27 14:29:27 +0900

    nav function / Contact Us complete

--- js/nav.js
+++ js/nav.js
@@ -8,10 +8,13 @@ const BOTTOM = document.querySelector('.bottom');
 
 const NAV_LIST = document.querySelector('.navbar-list');
 
-TOGGLE_BTN.addEventListener('click', ()=>{
+// Nav Func
+const navToggleFunc = ()=>{
     TOP.classList.toggle('toggle-active');
     MIDDLE.classList.toggle('toggle-deleted');
     BOTTOM.classList.toggle('toggle-active-two');
-
     NAV_LIST.classList.toggle('navbar-list-active');
-});
+}
+
+TOGGLE_BTN.addEventListener('click', navToggleFunc);
+
